ac67144fce4ca0a2164a731c02f9113835b7d38f
vanduxx11 <vandu8515@gmail.com>
2023-07-20 23:28:59 +0700

    migration

---
+++ app/Models/Product.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+    public function Category(){
+        return $this->belongsTo(Category::class);
+    }
+    public function orderItems(){
+      return $this->hasMany(OrderItem::class);
+    }
+}
